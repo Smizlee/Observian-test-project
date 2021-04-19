13567ce5aea4627555533410b9b959fe89cf6b30
Nick <nickmountz16@gmail.com>
2021-04-19 00:39:31 -0600

    css

--- test-project/src/components/header.jsx
+++ test-project/src/components/header.jsx
@@ -7,13 +7,11 @@ const Header = () => {
         <div className="headerContainer">
                 <div>
                     <img src={DarkRed} alt="DarkRed" />
-                    <div className="titleContainer">
-                        <text className="headerTitleText">NICK SAMPLE</text>
-                        <div className="underline"></div>
-                    </div>
-            
+                    <text className="headerTitleText">NICK SAMPLE</text>
+                    <div className="underline"></div>
+                    <img src={BlackLine} alt="BlackLine" className="blackLineImage" />
+                   
                 <div className="bodyContainer">
-                    <img src={BlackLine} alt="BlackLine" className="blackLineImage"/>
                     <div style={{width:"80%", alignSelf:'flex-end',margin:'80px'}}>
                         <p className="bodyText">One of the earliest activities we engaged in when we first got into astronomy is the same one we like to show our children just as soon as their excitement about the night sky begins to surface. That is the fun of finding constellations. But finding constellations and using them to navigate the sky is a discipline that goes back virtually to the dawn of man. In fact, we have cave pictures to show that the more primitive of human societies could “see pictures” in the sky and ascribe to them significance.</p>
                     </div>
